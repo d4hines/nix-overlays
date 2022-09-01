c1f4cbebbc35898443087927f0ab3efcadc541fc
UpdateBot <ulrikstrid@users.noreply.github.com>
2022-09-01 11:36:52 +0000

    Update sources

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ocaml-packages-overlay";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=c11d9597c1b3cdc4fb44cbab48deec2cfbaa5281";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=37d145eafc8023e0b46c94a50fa3f5a9f2ec4b48";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
